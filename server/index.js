c7ff77dd428f25d7e027ec18d4bc02a2c0f75b3a
szarina <zarina.saden@gmail.com>
2026-06-18 14:13:56 +0200

    wire auth and network routes into index.js, tested end to end

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import session from "express-session";
 import passport from "passport";
 import {Strategy as LocalStrategy} from "passport-local";
-import {getConnections, getStations, getUser, getUserById} from "./dao.js"
+import { getUser, getLines, getStations, getConnections, getSegments, createGame, submitRoute } from "./dao.js"
 
 // init express
 const app = express();
@@ -35,8 +35,8 @@ passport.use(new LocalStrategy(function verify(username, password, callback){
     getUser(username , password).then((user) => {
         if (!user)
             return callback(null, false, {message: 'Incorrect username or password'});
+        return callback(null, user);
     });
-
 }));
 
 passport.serializeUser(function (user, callback) {
@@ -47,33 +47,47 @@ passport.deserializeUser(function (user, callback) {
     return callback(null, user);
 })
 
+const isLoggedIn =(req,res,next) => {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    return res.status(401).json({error:'Not authenticated'})
+};
+
 // AUTH routes
 app.post('/api/sessions', passport.authenticate('local'), (req, res)=> {
     res.json(req.user);
 });
 
-app.delete('api/sessions/current', (req, res) => {
+app.delete('/api/sessions/current', (req, res) => {
     req.logout(()=> {
         res.end();
     });
 });
 
-app.get('apo/sessions/current', (req, res) => {
+app.get('/api/sessions/current', (req, res) => {
     if (req.isAuthenticated()){
-        req.json(req.user);
+        res.json(req.user);
     } else {
-        res.status(401).json({error: 'Not autenticated'});
+        res.status(401).json({error: 'Not authenticated'});
     }
 });
 
 // NETWORK
-app.get('/api/network', (req, res) =>{
+app.get('/api/network', isLoggedIn, (req, res) =>{
     Promise.all([getLines(), getStations(), getConnections()])
-            .then(([lines, stations, connections])=> {
-                res.json({lines, stations, connections});
-            })
-                .catch(() => res.status(500).end());
-        });
+        .then(([lines, stations, connections])=> {
+            res.json({lines, stations, connections});
+        })
+            .catch(() => res.status(500).end());
+});
+
+
+app.get('/api/segments', isLoggedIn,  (req, res) => {
+    getSegments()
+        .then((segments) => res.json(segments))
+        .catch(() => res.status(500).end());
+});
 
 
 
